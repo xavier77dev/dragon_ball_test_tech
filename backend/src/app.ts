a092b219a65d3db52cd3dfd48c53b1f7542aaf60
xavier77dev <xavier.basurto.77@gmail.com>
2024-09-06 05:54:16 -0500

    config: cors changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,10 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: '*',
+  origin: ['dragon-ball-test-tech-frontend.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 connectDB();
